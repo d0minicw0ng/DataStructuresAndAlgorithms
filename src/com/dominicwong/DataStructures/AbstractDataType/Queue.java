5c840b044e7d3b12d5998c76e5ca2d1b03bed734
Dominic Wong <dominic.wong.617@gmail.com>
2015-02-02 23:59:09 +0800

    Array Queue

---
+++ src/com/dominicwong/DataStructures/AbstractDataType/Queue.java
@@ -0,0 +1,48 @@
+package com.dominicwong.DataStructures.AbstractDataType;
+
+/**
+ * Created by dominicwong on 2/2/15.
+ */
+public class Queue {
+    private Object[] items;
+    private int head = 0;
+    private int tail = 0;
+    private int capactiy;
+    private int size = 0;
+
+    public Queue(int size) {
+        this.capactiy = size;
+        this.items = new Object[size];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == items.length - 1;
+    }
+
+    public void enqueue(Object data) throws RuntimeException {
+        if (isFull()) {
+            throw new RuntimeException("The queue is full");
+        } else {
+            items[tail] = data;
+            tail = (tail + 1) % capactiy;
+            size++;
+        }
+    }
+
+    public Object dequeue() throws RuntimeException {
+        if (isEmpty()) {
+            throw new RuntimeException("The queue is empty");
+        } else {
+            Object data = items[head];
+            items[head] = null;
+            head = (head + 1) % capactiy;
+            size--;
+            return data;
+        }
+    }
+
+}
--- src/com/dominicwong/Main.java
+++ src/com/dominicwong/Main.java
@@ -1,22 +1,5 @@
 package com.dominicwong;
 
-import com.dominicwong.DataStructures.LinkedList.LinkedList;
-import com.dominicwong.DataStructures.LinkedList.Node;
-
 public class Main {
-
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.insertHead(120);
-        list.insertHead(12);
-        list.insertHead(10);
-        list.insertHead(20);
-        list.insertHead(0);
-        list.insertHead(130);
-        list.insertHead(400);
-
-        list.getTail().setNext(list.getHead());
-        System.out.println(list.isCircular());
-
-    }
+    public static void main(String[] args) {}
 }
