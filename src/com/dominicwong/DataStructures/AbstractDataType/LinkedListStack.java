78aa3c31c92f45f071086d9259d024e5e133db4d
Dominic Wong <dominic.wong.617@gmail.com>
2015-02-03 21:50:01 +0800

    Stack based on Linked List

---
+++ src/com/dominicwong/DataStructures/AbstractDataType/LinkedListStack.java
@@ -0,0 +1,52 @@
+package com.dominicwong.DataStructures.AbstractDataType;
+
+import com.dominicwong.DataStructures.LinkedList.LinkedList;
+import com.dominicwong.DataStructures.LinkedList.Node;
+
+/**
+ * Created by dominicwong on 3/2/15.
+ */
+public class LinkedListStack {
+    private LinkedList list;
+
+    public LinkedListStack() {
+        this.list = new LinkedList();
+    }
+
+    public boolean isEmpty() {
+        return this.list.getHead() == null;
+    }
+
+    public void push(int data) {
+        if (isEmpty()) {
+            this.list.insertHead(data);
+        }
+
+        Node newNode = new Node(data);
+        Node current = this.list.getHead();
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(newNode);
+    }
+
+    public Node pop() throws RuntimeException {
+        if (isEmpty()) {
+            throw new RuntimeException("The stack is empty.");
+        }
+        Node prev = this.list.getHead();
+        Node current = prev.getNext();
+
+        if (current == null) {
+            this.list.deleteHead();
+            return prev;
+        }
+
+        while (current.getNext() != null) {
+            prev = prev.getNext();
+            current = current.getNext();
+        }
+        prev.setNext(null);
+        return current;
+    }
+}
